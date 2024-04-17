452c685bd332e9d47c3bce1e64c741ac963c5402
Chetandabbe <dabbechetan0@com>
2024-04-17 16:23:39 +0530

    Add multiple folders

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const bcrypt = require('bcrypt'); // Import bcrypt library
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
@@ -54,30 +55,6 @@ app.post('/submit', async (req, res) => {
         res.status(500).send('Internal Server Error');
     }
 });
-
-app.post('/staff_login', async (req, res) => {
-    const { username, password } = req.body;
-
-    try {
-        const user = await LoginData.findOne({ username });
-
-        if (user) {
-            
-            if (user.password===password) {
-                // res.send('<h1>Welcome to Login Page</h1>');
-                res.redirect('/admission_data');
-            } else {
-                res.send('Login unsuccessful: Incorrect password');
-            }
-        } else {
-            res.status(404).send('Login unsuccessful: Login Details not found');
-        }
-    } catch (error) {
-        console.error("Error logging in:", error);
-        res.status(500).send('Internal Server Error');
-    }
-});
-
 const loginSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -94,23 +71,22 @@ const loginSchema = new mongoose.Schema({
 const LoginData = mongoose.model('LoginData', loginSchema);
 
 module.exports = LoginData;
-
-app.post('/admin', async (req, res) => {
+app.post('/staff_login', async (req, res) => {
     const { username, password } = req.body;
 
     try {
+        const user = await LoginData.findOne({ username });
 
-        const admin = await AdminLoginData.findOne({ username });
-
-        if (admin) {
-
-            if (admin.password === password) {
-                res.redirect('/adduser');
+        if (user) {
+            const passwordCheck=await bcrypt.compare(password,user.password);
+            if (passwordCheck) {
+                // res.send('<h1>Welcome to Login Page</h1>');
+                res.redirect('/admission_data');
             } else {
-                res.status(401).send('Login unsuccessful: Incorrect password');
+                res.send('Login unsuccessful: Incorrect password');
             }
         } else {
-            res.status(404).send('Login unsuccessful: Admin not found');
+            res.status(404).send('Login unsuccessful: Login Details not found');
         }
     } catch (error) {
         console.error("Error logging in:", error);
@@ -138,6 +114,34 @@ const AdminLoginData = mongoose.model('AdminLoginData', AdminloginSchema);
 module.exports = AdminLoginData;
 
 
+
+app.post('/admin', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+
+        const admin = await AdminLoginData.findOne({ username });
+
+        if (admin) {
+
+            if (admin.password === password) {
+                res.redirect('/addUser');
+            } else {
+                res.status(401).send('Login unsuccessful: Incorrect password');
+            }
+        } else {
+            res.status(404).send('Login unsuccessful: Admin not found');
+        }
+    } catch (error) {
+        console.error("Error logging in:", error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
+
+
+
 app.get('/display', async (req, res) => {
     try {
         const data1 = await StudData.find({});
@@ -147,23 +151,30 @@ app.get('/display', async (req, res) => {
         res.status(500).send("Internal Server Error");
     }
 });
-app.post('/add',async(req,res)=>{
-    try{
-        const addNewUser=new LoginData({
+
+
+app.post('/add', async (req, res) => {
+    try {
+        
+        const hashedPassword = await bcrypt.hash(req.body.pass, 10); // 10 is the saltRounds
+        
+        const addNewUser = new LoginData({
             username: req.body.user,
-            password: req.body.pass,
+            password: hashedPassword, 
         });
         await addNewUser.save();
-        res.redirect('/');
-        // alert("Data saved successfully");
+        // alert("User added successfully");
+        res.redirect('/addUser');
+        
     }
-    catch(error)
-    {
-        res.status(404).send('Not found');
+    catch (error) {
+        console.error("Error adding user:", error);
+        res.status(500).send('Internal Server Error');
     }
-})
-app.get('/admission_data',(req,res) =>{
-    res.sendFile(path.join(__dirname,'public/Table_data/admission_history.html'))
+});
+
+app.get('/admission_data', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/Table_data/admission_history.html'))
 })
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -173,8 +184,8 @@ app.get('/login', async (req, res) => {
     res.sendFile(path.join(__dirname, 'public/staff_login/login.html'))
 });
 
-app.get('/adduser',async(req,res)=>{
-    res.sendFile(path.join(__dirname,'public/Add_user/add.html'))
+app.get('/adduser', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/Add_user/add.html'))
 })
 
 app.get('/admin_login', async (req, res) => {
